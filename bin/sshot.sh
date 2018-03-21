d66f575d5a13fc3d4edb840ba77e80bb50872ed6
malc <moosotc@gmail.com>
2018-03-21 21:16:57 +0300

    x11 screenshotting with ffmpeg

---
+++ bin/sshot.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+set -e
+test "$1" = "-" && {
+    id=$(xdotool getactivewindow)
+    shift
+} || id=$(xdotool selectwindow)
+
+eval $(xdotool windowactivate $id getwindowgeometry --shell $id)
+ffmpeg -hide_banner -loglevel panic \
+       -video_size "${WIDTH}x${HEIGHT}" \
+       -y -f x11grab -i $DISPLAY.$SCREEN+$X,$Y "$1"

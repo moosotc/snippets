c220a194a5ed399bdb56d2b5e6b3bec18d19fef0
malc <moosotc@gmail.com>
2018-03-21 23:41:35 +0300

    Uhm...

--- bin/sshot.sh
+++ bin/sshot.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -e
+
 test "$1" = "-" && {
     id=$(xdotool getactivewindow)
-    shift
-} || id=$(xdotool selectwindow)
+    out=${1-/tmp/shot.png}
+} || {
+    out=${1-/tmp/shot.png}
+    set -- $(xwininfo -root | grep "Window id:")
+    id=$4
+}
 
-eval $(xdotool windowactivate $id getwindowgeometry --shell $id)
-ffmpeg -hide_banner -loglevel panic \
-       -video_size "${WIDTH}x${HEIGHT}" \
-       -y -f x11grab -i $DISPLAY.$SCREEN+$X,$Y "$1"
+eval $(xdotool windowactivate $id getwindowgeometry --shell $id) || true
+ffmpeg -hide_banner -loglevel error                                          \
+       -video_size "${WIDTH}x${HEIGHT}"                                      \
+       -y -f x11grab -i $DISPLAY.$SCREEN+$X,$Y -vframes 1 "$out" || echo wtf

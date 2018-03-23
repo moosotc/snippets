672ec2e2ac267f23e382aca89b0f1803915aa96e
malc <moosotc@gmail.com>
2018-03-23 14:19:01 +0300

    better

--- bin/allmono.sh
+++ bin/allmono.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-find $PWD -type f | while read f; do
-    fc-scan -f '%{spacing}:\n' $f | grep -q '^[^:]' && echo $f
-done | while read f; do llppac -t font $f; done
+set -e
+
+path=${1-$PWD}
+find $path -type f | while read f; do
+    o=$(fc-scan "$f" -f "f='%{family}' s='%{spacing}' p='%{file}'") && {
+        eval $o
+        test -n "$s" -a -e "$p" -a -r "$p" && echo $p || true
+    }
+done

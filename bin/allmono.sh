a630b45046d05a7a163fb500098448a6213a8b19
malc <moosotc@gmail.com>
2018-03-23 17:30:38 +0300

    Bail out early
    Hope that family name does not contain '='.

--- bin/allmono.sh
+++ bin/allmono.sh
@@ -3,8 +3,9 @@ set -e
 
 path=${1-$PWD}
 find $path -type f | while read f; do
+    test -f "$f" || continue
     o=$(fc-scan "$f" -f 'f=\"%{family}\" s=\"%{spacing}\" p=\"%{file}\"') && {
         eval "$o"
-        test -n "$s" -a -e "$p" -a -r "$p" && echo $p || true
+        echo "$f=$p"
     }
 done

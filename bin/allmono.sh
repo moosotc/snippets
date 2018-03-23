1a4fdd7a1e18b65c9d4ab3c4e5ef305db8172f84
malc <moosotc@gmail.com>
2018-03-23 20:11:43 +0300

    Sigh.. -type f

--- bin/allmono.sh
+++ bin/allmono.sh
@@ -3,7 +3,6 @@ set -e
 
 path=${1-$PWD}
 find $path -type f | while read f; do
-    test -f "$f" || continue
     o=$(fc-scan "$f" -f 'f=\"%{family}\" s=\"%{spacing}\" p=\"%{file}\"') && {
         eval "$o"
         test -z $s || echo "$f=$p"

36429cbdbdf894f17bba2e6a6f6912b58207fa41
malc <moosotc@gmail.com>
2018-03-23 14:41:55 +0300

    Quoting

--- bin/allmono.sh
+++ bin/allmono.sh
@@ -3,8 +3,8 @@ set -e
 
 path=${1-$PWD}
 find $path -type f | while read f; do
-    o=$(fc-scan "$f" -f "f='%{family}' s='%{spacing}' p='%{file}'") && {
-        eval $o
+    o=$(fc-scan "$f" -f 'f=\"%{family}\" s=\"%{spacing}\" p=\"%{file}\"') && {
+        eval "$o"
         test -n "$s" -a -e "$p" -a -r "$p" && echo $p || true
     }
 done

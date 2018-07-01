e3979af1b68b33cbae6c414266a78114a1a86e1e
malc <moosotc@gmail.com>
2018-07-01 17:19:02 +0300

    Hmmm...
    Commented version results in division by zero...

--- bin/alttab.sh
+++ bin/alttab.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
 
-exec ~/x/rcs/git/alttab/src/alttab -w 1 -i 0x0 -t 512x64        \
-     -sc 1 -s 1 -font 'xft:iosevka malc-9'
+$HOME/x/rcs/git/alttab/src/alttab -font 'xft:iosevka malc-9' \
+                                  -t 512x64
+#-w 1 -i 0x0 -t 512x64 \
+#     -sc 1 -s 1 -font 'xft:iosevka malc-9'

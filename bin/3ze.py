2c40a66b2cdb2986d3054570ac86cda27fcd1cbb
malc <moosotc@gmail.com>
2020-05-30 16:34:52 +0300

    hmm

--- bin/3ze.py
+++ bin/3ze.py
@@ -4,8 +4,7 @@ import sys,fontforge
 if len (sys.argv) < 3:
     sys.exit (1)
 
-a = fontforge.open(sys.argv[1])               #Open a font
-a.selection.select (0x1b7)
+a = fontforge.open(sys.argv[1])
 a.selection.select (
     ord ('\N{LATIN CAPITAL LETTER EZH}')
 #    ord ('\N{LATIN CAPITAL LETTER YOGH}')
@@ -13,12 +12,18 @@ a.selection.select (
 )
 b = a
 a.copy ()
-b.fontname = a.fontname + " 3ze"
+b.fontname = a.fontname + "-3ze"
 b.selection.select("3")
+b.paste ()
+
+a.selection.select (ord ('\N{LATIN CAPITAL LETTER O WITH STROKE}'))
+a.copy ()
+b.selection.select("0")
 b.paste ()
 
 b.generate(sys.argv[2])
 
 # Local Variables:
-# compile-command: "python 3ze.py ~/x/fnt/NotoSans-Regular.ttf 3ze.ttf"
+# compile-command: "sh 3ze.sh ~/x/fnt/git-fonts/google-fonts/ofl/balsamiqsans/*ttf"
 # End:
+# "~/x/fnt/git-fonts/google-fonts/ofl/bellota/*ttf""

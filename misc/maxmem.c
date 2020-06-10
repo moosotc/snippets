5c0494b767b4fac484b7d5df73846cf003aaacd3
malc <moosotc@gmail.com>
2020-06-10 14:51:29 +0300

    better

--- misc/maxmem.c
+++ misc/maxmem.c
@@ -20,14 +20,15 @@ static unsigned long long maxmem (void)
         errno = 0;
         p = mmap (0, 1 << i,
                   PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-        printf ("alloc %u ret=%p: %s\n", 1 << i, p, strerror (errno));
+        if (0) printf ("alloc %u ret=%p: %s\n", 1 << i, p, strerror (errno));
         if (p == MAP_FAILED) {
             if (errno == ENOMEM) {
                 i -= 1;
             }
             else {
-                fprintf (stderr, "%s after allocating %lld\n",
-                         strerror (errno), total);
+                if (0)
+                    fprintf (stderr, "%s after allocating %lld\n",
+                             strerror (errno), total);
                 exit (EXIT_FAILURE);
             }
         }
@@ -76,9 +77,16 @@ int main (void)
     }
     memthr = maxmem ();
     maxmem ();
-    printf ("mem no thread %llu %gMb\n", memnothr, memnothr*1e-30);
-    printf ("mem thread %llu %lluMb\n", memthr, memthr>>20);
-    printf ("diff %llu %lluMb\n", memnothr - memthr,
-            (memnothr - memthr) >> 20);
+    printf ("mem no thread %llu %fGb\n", memnothr, memnothr*1e-9);
+    if ((memnothr - memthr) >> 20 > 8) {
+        printf ("mem thread %llu %lluMb\n", memthr, memthr>>20);
+        printf ("diff %llu %lluMb\n", memnothr - memthr,
+                (memnothr - memthr) >> 20);
+    }
     return 0;
 }
+/*
+  Local Variables:
+  compile-command: "gcc -o maxmem maxmem.c -lpthread"
+  End:
+*/

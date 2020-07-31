a33e97c64587cfd9beb81d7cea8a9551bd7dabca
malc <moosotc@gmail.com>
2020-07-31 16:52:49 +0300

    Trust no one
    hardcode values from
    https://www.displayspecifications.com/en/model/f72116de
    
    given that Xserver returned values thtat are way off (the calculated
    DPI (with the bogus demensions) yields 96 DPI in both directions)

--- misc/dpi.c
+++ misc/dpi.c
@@ -13,8 +13,15 @@ int main (void)
 
     win = wmm / 25.4;
     hin = hmm / 25.4;
+    win = 23.4709;
+    hin = 13.2024;
     wdpi = w / win;
     hdpi = h / hin;
     printf ("%dx%d %dx%d %f %f\n", w, h, wmm, hmm, wdpi, hdpi);
     return 0;
 }
+/*
+  Local Variables:
+  compile-command: "gcc -o dpi dpi.c -lX11 -g -Wall -Werror"
+  End:
+*/

19615d0283dfd0ac4a3aef8b162ba7ed00767c9d
malc <moosotc@gmail.com>
2020-08-20 04:07:11 +0300

    align

--- .config/fontconfig/gen.sh
+++ .config/fontconfig/gen.sh
@@ -95,24 +95,26 @@ M "pt sans narrow"    "BentonSansCond"
 M "lora"              "reithserif" "times, serif" "noto serif"
 M "$erif5"            "utopia" "domine"
 
-M "$mono2"         "ubuntu mono" "consolas" "courier" "courier new" "uimono"
-M "dudu cyryllic"  "comic sans ms" "sub"
-M "iosevka clam"   "iosevka" "monospace"
+M "$mono2"            "ubuntu mono" "consolas" "courier" "courier new" "uimono"
+M "dudu cyryllic"     "comic sans ms" "sub"
+M "iosevka clam"      "iosevka" "monospace"
 
-M "raleway-v4020"  "segoe ui" "arial" "roboto" "sans-serif" "sans serif"
-M "raleway-v4020"  "helvetica" "helvetica neue"
-M "kurinto Grga"   "georgia"
-M "pt astra serif" "times" "times new roman"
+M "raleway-v4020"     "segoe ui" "arial" "roboto" "sans-serif" "sans serif"
+M "raleway-v4020"     "helvetica" "helvetica neue"
+M "kurinto Grga"      "georgia"
+M "pt astra serif"    "times" "times new roman"
 
-M "$ans5"          "sans" "opensans" "noto sans" "verdana" "ubuntu"
-M "ruslan display" "cantarell" "tahoma"
+M "$ans5"             "sans" "opensans" "noto sans" "verdana" "ubuntu"
+
+M "ruslan display"    "cantarell" "tahoma"
+
+M "kurinto olde core" "constantia" "corbel" "candara"
+M "kurinto bria"      "cambria"
 
 M "kurinto olde core narrow" "calibri"
-M "kurinto olde core"        "constantia" "corbel" "candara"
-M "kurinto bria"             "cambria"
 
 # learn yourself some greek
-M "symbol"         "linux libertine"
+M "symbol" "linux libertine"
 
 echo "</fontconfig>"
 

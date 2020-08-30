f094ae107c798b8b36218c9ee74eee6788ee1ed7
malc <moosotc@gmail.com>
2020-08-30 06:40:42 +0300

    Remove kurinto grga core
    Фил

--- .config/fontconfig/gen.sh
+++ .config/fontconfig/gen.sh
@@ -104,7 +104,7 @@ M "iosevka clam"      "iosevka" "monospace"
 
 M "raleway-v4020"     "segoe ui" "arial" "roboto" "sans-serif" "sans serif"
 M "raleway-v4020"     "helvetica" "helvetica neue"
-M "kurinto Grga"      "georgia"
+M "beoclah"           "georgia"
 M "pt astra serif"    "times" "times new roman"
 
 M "$ans5"             "sans" "opensans" "noto sans" "verdana" "ubuntu"

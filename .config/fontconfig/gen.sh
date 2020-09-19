a1f6d605e11e8ce5f0959737abb4ef3086f0705c
malc <moosotc@gmail.com>
2020-09-19 20:28:46 +0300

    one more bbc tweak

--- .config/fontconfig/gen.sh
+++ .config/fontconfig/gen.sh
@@ -110,7 +110,7 @@ M "dudu cyryllic"     "comic sans ms" "sub"
 M "iosevka clam"      "iosevka" "monospace"
 
 M "raleway-v4020"     "segoe ui" "arial" "roboto" "sans-serif"
-M "raleway-v4020"     "helvetica neue" "cantarell" "freesans"
+M "raleway-v4020"     "helvetica neue" "cantarell" "freesans" "reithsans"
 M "pt astra serif"    "times" "times new roman"
 
 M "$ans5"             "opensans" "open sans" "noto sans" "verdana" "ubuntu"

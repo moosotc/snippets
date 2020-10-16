46817c541c4e365c7ceb922c32406fe3676fbfc2
malc <moosotc@gmail.com>
2020-10-17 01:28:48 +0300

    :(

--- .config/fontconfig/gen.sh
+++ .config/fontconfig/gen.sh
@@ -102,7 +102,7 @@ M "dudu cyryllic"  "comic sans ms" "sans" "sub"
 M "iosevka clam"   "iosevka" "monospace"
 M "pt astra serif" "serif" "times" "times new roman"
 M "ruslan display" "sans serif"
-eval M "'$ans'"    "verdana" "'lucida grande'" "helvetica" $dsans
+eval M "'$ans5'"   "verdana" "'lucida grande'" "helvetica" $dsans
 M "$erif"          "utopia" "domine" "pt serif" "georgia"
 M "$kuri"          "cantarell" "tahoma" "ubuntu"
 M "$curs"          "cursive"

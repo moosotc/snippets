92d1a577c73c701f114a8be891f4fc629fb782cd
malc <moosotc@gmail.com>
2020-11-06 07:12:18 +0300

    Avoid password (re)entry

--- bin/ddc.sh
+++ bin/ddc.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-
-. asroot
 set -e
 trap 'test $? != 0 && echo ret=$?' EXIT
+
 n=6
 p=/dev/i2c-$n
-test -c $p || modprobe i2c-dev
-test -w $p || {
-    chgrp wheel $p
-    chmod g+rw $p
+test -c $p || {
+    sudo modprobe i2c-dev
+    sudo chgrp wheel $p
+    sudo chmod g+rw $p
 }
 exec ddcutil --bus 6 $*

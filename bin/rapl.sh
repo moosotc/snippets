233f81ae4b72be390d51cee9326f2f9eced98bfa
malc <clamky@hotmail.com>
2020-11-23 22:51:12 +0300

    no idea

--- bin/rapl.sh
+++ bin/rapl.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-n=${1:-$(id -un)}
+set -x
+n=${1:-$(id -u)}
 . asroot $n
-chown $n /sys/devices/virtual/powercap/intel-rapl/intel-rapl*/energy_uj
+chown $n /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/intel-rapl:*/energy_uj

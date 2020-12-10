c7e1b5b7e37c56367f612a46a65134c88255794e
malc <clamky@hotmail.com>
2020-12-10 12:58:02 +0300

    less fancy, more better

--- bin/rapl.sh
+++ bin/rapl.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 set -x
-n=${1:-$(id -u)}
 . asroot $n
-#chown $n /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/intel-rapl:*/energy_uj
-chown $n /sys/class/powercap/intel-rapl:*/energy_uj
+chmod a+r /sys/class/powercap/**/*/energy_uj

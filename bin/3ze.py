d796c62ee650294535c45c319b94728ea8aff98f
malc <clamky@hotmail.com>
2021-06-04 14:20:47 +0300

    3

--- bin/3ze.py
+++ bin/3ze.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
-import sys,fontforge
+import sys, fontforge
 
 try:
     if sys.argv[1] == '-0':
         o0 = True
         sys.argv = sys.argv[1:]
+        sys.argv.remove (1)
     else:
         o0 = False
 except:
     sys.exit (1)
 
 a = fontforge.open(sys.argv[1])
-a.selection.select (
-#    ord ('\N{LATIN SMALL LETTER EZH}'
-    ord ('\N{CYRILLIC CAPITAL LETTER ABKHASIAN DZE}')
-#    ord ('\N{LATIN CAPITAL LETTER YOGH}')
-#    ord ('\N{LATIN SMALL LETTER YOGH}') # descends beyond baselin
-)
+#ch = ord ('\N{LATIN CAPITAL LETTER YOGH}')
+#ch = ord ('\N{LATIN SMALL LETTER YOGH}') # descends beyond baselin
+ch = ord ('\N{LATIN SMALL LETTER EZH}')
+#ch = ord ('\N{CYRILLIC CAPITAL LETTER ABKHASIAN DZE}')
+a.selection.select (ch)
 
 b = a
 a.copy ()
 b.selection.select("3")
 b.paste ()
 
+# 3ze.sh can't pass arguments down here so...
+# o0 = True
 if o0:
     a.selection.select (ord ('\N{LATIN CAPITAL LETTER O WITH STROKE}'))
     a.copy ()

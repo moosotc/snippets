f0eac0126e46c8f125cd1ef186f68c8069301baa
malc <clamky@hotmail.com>
2021-06-21 01:44:38 +0300

    remove

--- bin/3ze-lato.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-py=$(dirname $0)/3ze.py
-for f in "$@"; do
-    t=$(basename "$f")
-    python $py -3 -0 -l "$f" "${t%.*}.ttf"
-done
--- bin/3ze.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-py=$(dirname $0)/3ze.py
-for f in "$@"; do
-    t=$(basename "$f")
-    python $py -3 -l -0 "$f" ze3-$t
-done

0634f224c932554094006b265b0e9f5ba490f43d
malc <clamky@hotmail.com>
2021-06-25 07:23:09 +0300

    elit

--- misc/mutilator/noto-sans.py
+++ misc/mutilator/noto-sans.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# for f in $(find ~/x/fnt/fix/noto -name '*ttf'); do python ~/xsrc/snippets/misc/mutilator/noto-sans.py $f $(basename $f); done
+"""
+for f in ~/x/fnt/files/noto/*.ttf; do
+  python ~/xsrc/snippets/misc/mutilator/noto-sans.py $f $(basename $f)
+done
+"""
 
 import sys, fontforge
 
@@ -21,5 +25,17 @@ if False:
     a.copy ()
     b.selection.select('l')
     b.paste ()
+
+# LATIN SMALL LETTER L WITH RETROFLEX HOOK
+# uni1DA9 MODIFIER LETTER SMALL L WITH RETROFLEX HOOK
+# uni1DA5 MODIFIER LETTER SMALL IOTA
+a.selection.select ('iotaLatin')
+# a.selection.select (ord ('\N{LATIN SMALL LETTER L WITH RETROFLEX HOOK}'))
+# --- a.selection.select ('uni1DA9')
+# --- a.selection.select ('uni1DA5')
+
+a.copy ()
+b.selection.select('l')
+b.paste ()
 
 b.generate(sys.argv[2], flags=("opentype", "no-hints"))

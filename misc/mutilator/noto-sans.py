19e6cc26c6411ce1a3ec5281ddbb0e94e04be540
malc <clamky@hotmail.com>
2021-06-25 07:45:28 +0300

    settle on iota for now

--- misc/mutilator/noto-sans.py
+++ misc/mutilator/noto-sans.py
@@ -20,19 +20,8 @@ a.copy ()
 b.selection.select("0")
 b.paste ()
 
-if False:
-    a.selection.select (ord ('ɭƖꙆꙇ'[0]))
-    a.copy ()
-    b.selection.select('l')
-    b.paste ()
-
-# LATIN SMALL LETTER L WITH RETROFLEX HOOK
-# uni1DA9 MODIFIER LETTER SMALL L WITH RETROFLEX HOOK
-# uni1DA5 MODIFIER LETTER SMALL IOTA
 a.selection.select ('iotaLatin')
 # a.selection.select (ord ('\N{LATIN SMALL LETTER L WITH RETROFLEX HOOK}'))
-# --- a.selection.select ('uni1DA9')
-# --- a.selection.select ('uni1DA5')
 
 a.copy ()
 b.selection.select('l')

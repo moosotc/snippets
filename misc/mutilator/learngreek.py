0ca63a27d6367a62232dcd166edabcae3bf5c3e6
malc <clamky@hotmail.com>
2021-06-26 19:41:33 +0300

    step by step

---
+++ misc/mutilator/learngreek.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import sys, fontforge
+"""
+for f in ~/x/fnt/files/noto/serif/*.ttf; do
+  python ~/xsrc/snippets/misc/mutilator/learngreek.py $f $(basename $f)
+done
+"""
+
+# https://en.wikipedia.org/wiki/Greek_alphabet
+xtbl = {
+    'α' : 'a',
+    'ꞵ' : 'b',
+    'γ' : 'g',
+    'δ' : 'd',
+    'ε' : 'e',
+    'ζ' : 'z'
+}
+
+a = fontforge.open (sys.argv[1])
+b = a
+for s,d in xtbl.items():
+    a.selection.select (ord (s))
+    a.copy ()
+
+    b.selection.select (ord (d))
+    b.paste ()
+
+a.selection.select ('zero.slash')
+a.copy ()
+b.selection.select ("0")
+b.paste ()
+
+a.selection.select (ord ('\N{CYRILLIC CAPITAL LETTER ABKHASIAN DZE}')) # Ӡ
+a.copy ()
+b.selection.select ("3")
+b.paste ()
+
+b.generate(sys.argv[2], flags=("opentype", "no-hints"))

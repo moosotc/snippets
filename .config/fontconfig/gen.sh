b6e5c1a8c602dc172136329ed4d7edd69dd1a133
malc <clamky@hotmail.com>
2021-07-08 17:21:30 +0300

    add and use an alias for small mono

--- .config/fontconfig/gen.sh
+++ .config/fontconfig/gen.sh
@@ -9,6 +9,7 @@ set -eu
 
 # montserrat alternates https://github.com/JulietaUla/Montserrat
 # fantasque sans mono   https://github.com/belluzj/fantasque-sans
+# ubuntu                https://design.ubuntu.com/font/
 # noto sans             https://noto-website-2.storage.googleapis.com/pkgs/NotoSans-unhinted.zip
 # noto serif            https://noto-website-2.storage.googleapis.com/pkgs/NotoSerif-unhinted.zip
 # jetbrains mono        https://github.com/JetBrains/JetBrainsMono
@@ -40,7 +41,7 @@ cat <<EOF
   </match>
 EOF
 
-mono0="fantasque sans mono"
+mono0="ubuntu mono"
 mono2="jetbrains mono"
 sans1="noto sans"
 sans2="montserrat alternates"
@@ -49,7 +50,7 @@ fallb="Last Resort High-Efficiency"
 
 M "$sans1" "opensans" "open sans" "noto sans" "droid sans"
 M "$sans1" "sans" "sans-serif"
-M "$mono0" "courier new" "monaco" "consolas"
+M "$mono0" "mono0 ""courier new" "monaco" "consolas"
 M "$mono2" "monospace" "bitstream vera sans mono"
 M "$sans2" "vera" "lucida grande" "verdana"
 M "$serif" "serif"

a245df807e14de4d3be6066a57360f0f18c187a6
malc <clamky@hotmail.com>
2021-07-14 02:13:03 +0300

    Crime description

--- misc/mutilator/montserrat-alternates.py
+++ misc/mutilator/montserrat-alternates.py
@@ -4,4 +4,7 @@
 for f in ~/x/fnt/git/Montserrat/fonts/ttf/*Alt*-{Bold,BoldItalic,Italic,Regular}.ttf; do
   cp $f .
 done
+
+And then placed middle dot (0xb7) into zero (0x30) by hand in those
+files, telling fontforge to generate font files...
 """

58243a785a2862c86578821595343db63c929421
malc <clamky@hotmail.com>
2021-07-13 09:24:21 +0300

    this

--- misc/mutilator/montserrat-alternates.py
+++ misc/mutilator/montserrat-alternates.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-for f in ~/x/fnt/git/Montserrat/fonts/otf/*Alt*-{Bold,Light}*; do
-  python ~/xsrc/snippets/misc/mutilator/montserrat-alternates.py $f $(basename $f)
+for f in ~/x/fnt/git/Montserrat/fonts/ttf/*Alt*-{Bold,BoldItalic,Italic,Regular}.ttf; do
+  cp $f .
 done
 """
-
-import sys, fontforge
-
-a = fontforge.open (sys.argv[1])
-a.selection.select (ord ('\N{LATIN CAPITAL LETTER O WITH STROKE}')) # Ø
-b = a
-a.copy ()
-b.selection.select ("0")
-b.paste ()
-
-b.generate(sys.argv[2], flags=("opentype", "no-hints"))
